69ee81b35ebc55330d365914c31b4de144c68b15
max <jiwei0706@gmail.com>
2025-05-01 15:46:54 -0700

    new data script

--- start_joycon.sh
+++ start_joycon.sh
@@ -3,4 +3,5 @@ source ~/miniconda3/bin/activate
 conda activate omnigibson
 python reboot_dynamixel.py 
 
-python experiments/run_r1_gello.py --joint_config_file joint_config_black_gello_newest.yaml
+python experiments/run_joylo.py --gello_model r1pro --joint_config_file joint_config_joylo.yaml
+
