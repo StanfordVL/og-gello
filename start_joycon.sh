4a858188acbaaa4b470b9224686e36631e0b0e9e
Wei Ji <jiwei0706@gmail.com>
2025-05-09 12:28:44 -0700

    revise start scripts

--- start_joycon.sh
+++ start_joycon.sh
@@ -3,5 +3,5 @@ source ~/miniconda3/bin/activate
 conda activate omnigibson
 python reboot_dynamixel.py 
 
-python experiments/run_joylo.py --gello_model r1pro --joint_config_file joint_config_joylo.yaml
+python experiments/run_joylo.py --joint_config_file joint_config_joylo.yaml
 
